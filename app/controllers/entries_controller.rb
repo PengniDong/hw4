2433ab1a0ad393d5ebe18775eac23b51e9c3346e
Pengni Dong <pengni.dong@kellogg.northwestern.edu>
2025-03-17 03:57:41 +0000

    change

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,14 +1,18 @@
 class EntriesController < ApplicationController
 
   def new
+    @entry = Entry.new
+    @entry.place_id = params["place_id"]
   end
-
+  
   def create
     @entry = Entry.new
-    @entry["title"] = params["title"]
-    @entry["description"] = params["description"]
-    @entry["occurred_on"] = params["occurred_on"]
-    @entry["place_id"] = params["place_id"]
+    @entry["title"] = params["entry"]["title"]
+    @entry["description"] = params["entry"]["description"]
+    @entry["occurred_on"] = params["entry"]["occurred_on"]
+    @entry["place_id"] = params["entry"]["place_id"]
+    @entry["user_id"] = @current_user.id if @current_user
+    @entry.image.attach(params["entry"]["image"]) if params["entry"]["image"]
     @entry.save
     redirect_to "/places/#{@entry["place_id"]}"
   end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(email: params["email"])
+    if @user && BCrypt::Password.new(@user.password) == params["password"]
+      session["user_id"] = @user.id
+      redirect_to "/places"
+    else
+      redirect_to "/sessions/new"
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to("/", notice: "Logged out.")
   end
 end
   
