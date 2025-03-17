4f2efbefc592adc21e6c97ae822bfff358b21624
Pengni Dong <pengni.dong@kellogg.northwestern.edu>
2025-03-17 21:17:03 +0000

    edit1

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,17 +1,22 @@
 class PlacesController < ApplicationController
-
   def index
     @places = Place.all
   end
-
+  
   def show
     @place = Place.find_by({ "id" => params["id"] })
-    @entries = Entry.where({ "place_id" => @place["id"] })
+    
+    # Only show entries for the logged-in user
+    if @current_user
+      @entries = Entry.where({ "place_id" => @place["id"], "user_id" => @current_user["id"] })
+    else
+      @entries = []
+    end
   end
-
+  
   def new
   end
-
+  
   def create
     @place = Place.new
     @place["name"] = params["name"]
